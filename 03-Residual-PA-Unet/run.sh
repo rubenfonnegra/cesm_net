c533eb012319ff2b674abc390fa3c6c55d2c17a8
Kevin Osorno Castillo <kevinosornocastillo@gmail.com>
2023-02-10 07:31:48 -0500

    Exp data without black background and edges

--- 03-Residual-PA-Unet/run.sh
+++ 03-Residual-PA-Unet/run.sh
@@ -8,17 +8,29 @@ python3 run_models.py   --gpus 0 \
                         --dataset_name cesm \
                         --result_dir /media/labmirp/Datos/workspaces/cesm_net/Results/03-Residual-Pixel-Attention-Unet/ \
                         --projection CC \
-                        --exp_name residual-PA-unet-original \
-                        --data_dir /media/labmirp/Datos/workspaces/cesm_net/Data/cesm_patches/original/ \
+                        --exp_name residual-PA-unet-data-without-bg \
+                        --data_dir /media/labmirp/Datos/workspaces/cesm_net/Data/cesm_patches/data_without_background/ \
                         --image_size 256 \
                         --channels 1 \
-                        --batch_size 5 \
-                        --n_epochs 100 \
+                        --batch_size 30 \
+                        --n_epochs 401 \
                         --workers 12 \
-                        --sample_interval 10 \
+                        --sample_interval 50 \
                         --checkpoint_interval 50 \
-                        --model UNet
-#python3 run_models.py --gpus 0 --generate --epoch 200 --exp_name unet_CC_bg_30_01 --sample_size 50 --dataset_name cesm --projection CC --data_dir Data/cesm_patches/borde_bg_30/ --image_size 256 --channels 1
+                        --model UNet \
+                        --use_wandb True 
+
+python3 run_models.py   --gpus 0 \
+                        --generate \
+                        --epoch 400 \
+                        --exp_name residual-PA-unet-data-without-bg \
+                        --sample_size 50 \
+                        --dataset_name cesm \
+                        --projection CC \
+                        --data_dir /media/labmirp/Datos/workspaces/cesm_net/Data/cesm_patches/data_without_background/ \
+                        --image_size 256 \
+                        --channels 1 \
+                        --use_wandb False \
 
 # Experiment Unet MLO
 #python3 run_models.py --gpus 0 --dataset_name cesm --projection MLO --exp_name unet_MLO_bg --data_dir Data/cesm_patches/borde_bg_20/ --image_size 256 --channels 1 --batch_size 30 --n_epochs 201 --workers 12 --sample_interval 100 --checkpoint_interval 50 --model UNet
