a0dc87b9ec34350f1745eb73926eb69d932ad3a0
Kevin Osorno Castillo <kevinosornocastillo@gmail.com>
2023-03-07 12:08:36 -0500

    Data extraction configuration

--- 01-Data-Extraction/data_tool.sh
+++ 01-Data-Extraction/data_tool.sh
@@ -1,11 +1,31 @@
-source /home/mirplab/Documents/kevin/venvs/cesm_venv/bin/activate
-cd /home/mirplab/Documents/kevin/01-cesm_net/01-Data-Extraction/
+source /media/labmirp/Datos/venvs/cesm_env/bin/activate
+cd /media/labmirp/Datos/workspaces/cesm_net/01-Data-Extraction/
 
 
-python3 data_tool.py    --pr_id "CC" \
-                        --b_id "L" \
-                        --name "sura_crop_image" \
-                        --name_data "cesm" \
-                        --path_data 
-                        --plot_resize
-python3 data_tool.py --pr_id "MLO" --name "cdd_cesm_full_image" --name_data "cdd-cesm" --plot_resize
+python3 data_tool.py --pr_id CC \
+                    --b_id L \
+                    --name sura_crop_image \
+                    --name_data cesm \
+                    --plot_resize \
+                    --crop
+
+python3 data_tool.py --pr_id CC \
+                    --b_id R \
+                    --name sura_crop_image \
+                    --name_data cesm \
+                    --plot_resize \
+                    --crop \
+
+python3 data_tool.py --pr_id MLO\
+                    --b_id L \
+                    --name sura_crop_image \
+                    --name_data cesm \
+                    --plot_resize \
+                    --crop \
+
+python3 data_tool.py --pr_id MLO \
+                    --b_id R\
+                    --name sura_crop_image \
+                    --name_data cesm\
+                    --plot_resize \
+                    --crop \
