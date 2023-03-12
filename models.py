015949034f04305d0da2f15cff2c35b7e2ef03f0
Kevin Osorno Castillo <kevinosornocastillo@gmail.com>
2023-03-12 16:27:20 -0500

    Configuration for exp PA-Unet

--- models.py
+++ models.py
@@ -247,16 +247,20 @@ class PA_UNet_Generator(nn.Module):
         )
 
         """ DownSampling Block """
-        self.RB1 = R_block(32,32)       
+        self.RB1 = R_block(32,32)
+        self.PA1 = PixelAttention(32)       
         self.DS1 = DS_block(32,64)                
-        self.RB2 = R_block(64,64)       
+        self.RB2 = R_block(64,64)
+        self.PA2 = PixelAttention(64)      
         self.DS2 = DS_block(64,128)
-        self.PA1 = PixelAttention(128)   
-        self.RB3 = R_block(128,128)     
+        self.RB3 = R_block(128,128)
+        self.PA3 = PixelAttention(128)     
         self.DS3 = DS_block(128,256)
         self.RB4 = R_block(256,256)
+        self.PA4 = PixelAttention(256)
         self.DS4 = DS_block(256,512)
         self.RB5 = R_block(512,512)
+        self.PA5 = PixelAttention(512)
         self.DS5 = DS_block(512,1024)
         
         """ Fusion Block """
@@ -296,17 +300,21 @@ class PA_UNet_Generator(nn.Module):
     def forward(self, img_input):
         
         """ DownSampling Block Forward """
-        outConvInit     = self.convInput(img_input) # (B, 32, 256,256)
+        outConvInit     = self.convInput(img_input)     # (B, 32, 256,256)
         outRB1          = self.RB1(outConvInit)         # (B, 32, 256,256)
+        outPA1, attn1   = self.PA1(outRB1)              # (B, 32, 256, 256)
         outDS           = self.DS1(outRB1)              # (B, 64, 128,128)
         outRB2          = self.RB2(outDS)               # (B, 64, 128,128)
+        outPA2, attn2   = self.PA2(outRB2)              # (B, 64, 128, 128)
         outDS           = self.DS2(outRB2)              # (B, 128, 64, 64)
         outRB3          = self.RB3(outDS)               # (B, 128, 64, 64)
-        outPA1, attn1   = self.PA1(outRB3)              # (B, 128, 64, 64)
-        outDS           = self.DS3(outPA1)              # (B, 256, 32, 32)
+        outPA3, attn3   = self.PA3(outRB3)              # (B, 128, 64, 64)
+        outDS           = self.DS3(outRB3)              # (B, 256, 32, 32)
         outRB4          = self.RB4(outDS)               # (B, 256, 32, 32)
+        outPA4, attn4   = self.PA4(outRB4)              # (B, 256, 32, 32)
         outDS           = self.DS4(outRB4)              # (B, 512, 16, 16)
         outRB5          = self.RB5(outDS)               # (B, 512, 16, 16)
+        outPA5, attn5   = self.PA5(outRB5)              # (B, 512, 16, 16)
         outDS           = self.DS5(outRB5)              # (B, 1024, 8, 8)
 
         """ Fusion Block Forward """
@@ -315,15 +323,15 @@ class PA_UNet_Generator(nn.Module):
         out = self.reluFusion(out)              # (B, 1024, 8, 8)
 
         """ Upsampling Block Forward """
-        out = self.US1( out, outRB5 )           # (B, 512, 16, 16)
+        out = self.US1( out, outPA5 )           # (B, 512, 16, 16)
         out = self.RB6( out )                   # (B, 512, 16, 16)
-        out = self.US2( out, outRB4 )           # (B, 256, 32, 32)
+        out = self.US2( out, outPA4 )           # (B, 256, 32, 32)
         out = self.RB7( out )                   # (B, 256, 32, 32)
-        out = self.US3(out, outPA1 )            # (B, 128, 64, 64)
+        out = self.US3(out, outPA3 )            # (B, 128, 64, 64)
         out = self.RB8(out)                     # (B, 128, 64, 64)
-        out = self.US4(out, outRB2 )            # (B, 64, 128, 128)
+        out = self.US4(out, outPA2 )            # (B, 64, 128, 128)
         out = self.RB9(out)                     # (B, 64, 128, 128)
-        out = self.US5(out, outRB1 )            # (B, 32, 256, 256)
+        out = self.US5(out, outPA1 )            # (B, 32, 256, 256)
         out = self.RB10(out)                    # (B, 32, 256, 256)
 
         """ Output Convolution """
@@ -334,6 +342,10 @@ class PA_UNet_Generator(nn.Module):
 
             "image_input": img_input,
             "attn1": attn1,
+            "attn2": attn2,
+            "attn3": attn3,
+            "attn4": attn4,
+            "attn5": attn5,
             "output_image": out,
         }
 
